4f41a63b6c6897395b7923d5becf1c951fd570fe
Yu357 <forestgreen14689@gmail.com>
2023-01-29 11:32:20 +0900

    feat: APIのURLを変更

--- src/utilities/APIService.ts
+++ src/utilities/APIService.ts
@@ -5,7 +5,7 @@ class APIService {
   static async helloAPI(): Promise<string | null> {
 
     return axios
-      .get('http://34.204.60.163:8000/')
+      .get('http://localhost:8000')
       .then(response => {
 
         return response.data
