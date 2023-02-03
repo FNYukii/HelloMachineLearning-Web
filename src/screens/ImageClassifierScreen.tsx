6c6b61376301816bfc7e3fa7edd50105b83bfc4c
Yu357 <forestgreen14689@gmail.com>
2023-02-03 12:19:53 +0900

    feat: post通信の実装作業中

--- src/screens/ImageClassifierScreen.tsx
+++ src/screens/ImageClassifierScreen.tsx
@@ -1,22 +1,59 @@
+import axios from "axios"
 import { useState } from "react"
 import PickImageButton from "../components/PickImageButton"
 import Screen from "../components/Screen"
 
 function ImageClassifierScreen() {
 
-	// eslint-disable-next-line
-  const [image, setImage] = useState<File | null>(null)
+	const [image, setImage] = useState<File | null>(null)
+	const [result, setResult] = useState("")
+	const [isLoading, setIsLoading] = useState(false)
 
-  return (
-    <Screen title="Image Classifier">
+	async function classificate() {
 
-      <p>画像に写っているものがどのカテゴリに属するものなのか分類します。</p>
+		setIsLoading(true)
 
-      <PickImageButton setPickedImage={setImage} />
+		axios.post(`${process.env.REACT_APP_API_DOMAIN}`, image, {
+				headers: {
+					'Content-Type': 'multipart/form-data',
+				},
+			})
+			.then(response => {
 
-      <button type="button" className="mt-2 py-1 px-4 rounded-full border border-gray-500 text-gray-500 hover:bg-gray-200 transition">Submit</button>
-    </Screen>
-  )
+				setIsLoading(false)
+				setResult(response.data)
+			})
+			.catch((error) => {
+
+				setIsLoading(false)
+				setResult(`${error}`)
+			})
+	}
+
+	return (
+		<Screen title="Image Classifier">
+
+			<p>画像に写っているものがどのカテゴリに属するものなのか分類します。</p>
+
+			<PickImageButton setPickedImage={setImage} />
+
+			<button type="button" onClick={classificate} className="mt-2 py-1 px-4 rounded-full border border-gray-500 text-gray-500 hover:bg-gray-200 transition">Submit</button>
+
+			<div className="mt-2">
+
+				{isLoading &&
+					<p>Loading...</p>
+				}
+
+				{!isLoading &&
+					<p>
+						<span className="text-gray-500">結果: </span>
+						<span>{result}</span>
+					</p>
+				}
+			</div>
+		</Screen>
+	)
 }
 
 export default ImageClassifierScreen
