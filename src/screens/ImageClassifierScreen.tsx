b4557d709f45ac02b7de74399cb036da3210797b
Yu357 <forestgreen14689@gmail.com>
2023-02-03 10:06:05 +0900

    clean: 不要な機能を削除

--- src/screens/ImageClassifierScreen.tsx
+++ src/screens/ImageClassifierScreen.tsx
@@ -1,17 +1,10 @@
 import { useState } from "react"
 import PickImageButton from "../components/PickImageButton"
 import Screen from "../components/Screen"
-import APIService from "../utilities/APIService"
 
 function ImageClassifierScreen() {
 
   const [image, setImage] = useState<File | null>(null)
-
-  async function hello() {
-    const result = await APIService.helloAPI()
-
-    alert(`結果: ${result}`)
-  }
 
   return (
     <Screen title="Image Classifier">
@@ -20,7 +13,7 @@ function ImageClassifierScreen() {
 
       <PickImageButton setPickedImage={setImage} />
 
-      <button type="button" onClick={hello} className="mt-2 py-1 px-4 rounded-full border border-gray-500 text-gray-500 hover:bg-gray-200 transition">Submit</button>
+      <button type="button" className="mt-2 py-1 px-4 rounded-full border border-gray-500 text-gray-500 hover:bg-gray-200 transition">Submit</button>
     </Screen>
   )
 }
